8f85c58ed1a1288f560fbfe31cd8d0292f7608d1
dinkar.vashishat <dinkar.vashishat.ext@siemens-energy.com>
2024-03-11 23:48:25 +0530

    initial setup commits

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # aws_bedrock
 Learning project with python to use AWS-Bedrock
+
+# All the aws information will be stored in this file
+aws username = div@fullstack
---
+++ initial-file.py
@@ -0,0 +1,21 @@
+import boto3
+
+iam_client = boto3.client('iam')
+
+# Get the user name or role ARN (depending on your scenario)
+user_name = "div@fullstack"
+
+# List attached policies (ARNs)
+attached_policies = iam_client.list_attached_role_policies(RoleName=user_name)["AttachedPolicies"] if ":" in user_name else iam_client.list_attached_user_policies(UserName=user_name)["AttachedPolicies"]
+
+# (Optional) Get inline policy (if applicable)
+inline_policy = iam_client.get_user_policy(UserName=user_name) if ":" not in user_name else None
+
+# Print the retrieved information
+print("Attached Policies:")
+for policy in attached_policies:
+    print(f"\t- {policy['PolicyArn']}")
+
+if inline_policy:
+    print("Inline Policy:")
+    print(inline_policy["PolicyDocument"])
